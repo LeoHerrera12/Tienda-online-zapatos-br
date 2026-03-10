d1b3b86d73078caedd2edec261b46c6fec559c93
LeoHerrera12 <lhv.2606.lh@gmail.com>
2026-03-10 00:44:13 -0400

    Add app.js with Boutique app JavaScript code

---
+++ app.js
@@ -0,0 +1,11 @@
+// JavaScript code for the boutique app
+
+console.log('Welcome to the Boutique App!');
+
+// Example function to add an item to the cart
+function addItemToCart(item) {
+    console.log(`Added ${item} to cart.`);
+}
+
+// Example usage
+addItemToCart('Shoes');
